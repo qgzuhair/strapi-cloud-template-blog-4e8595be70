a0ff77b13ffb32b3305d1a4c220b656a8fc1384a
qgzuhair <zuhair.naqvi@qualitasglobal.com>
2025-11-17 12:44:17 +0530

    Updated Email Configration

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,18 +2,19 @@ module.exports = {
   email: {
     config: {
       provider: 'nodemailer',
-      providerOptions: {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        auth: {
-          user: process.env.SMTP_USERNAME,
-          pass: process.env.SMTP_PASSWORD,
-        },
-        secure: Number(process.env.SMTP_PORT) === 587,
-        tls: {
-          rejectUnauthorized: false, // <- IMPORTANT fix for cPanel / custom servers
-        },
-      },
+     providerOptions: {
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false, // TLS (not SSL)
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+},
+
       settings: {
         defaultFrom: process.env.SMTP_FROM,
         defaultReplyTo: process.env.SMTP_REPLY_TO,
