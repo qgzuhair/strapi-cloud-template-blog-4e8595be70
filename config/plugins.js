c17291fac8363c9bdfbc53f365454d5cf95a1053
qgzuhair <zuhair.naqvi@qualitasglobal.com>
2025-11-18 16:27:47 +0530

    Updated Email Configration

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,23 @@
 module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: 'md-ht-1.webhostbox.net',
-        port: 587,
-        secure: false, // TLS, NOT SSL
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-        tls: {
-          rejectUnauthorized: false, // cPanel requires this
-        },
-      },
-      settings: {
-        defaultFrom: env('SMTP_FROM'),
-        defaultReplyTo: env('SMTP_REPLY_TO'),
-      },
-    },
-  },
+  // email: {
+  //   config: {
+  //     provider: 'nodemailer',
+  //     providerOptions: {
+  //       host: 'md-ht-1.webhostbox.net',
+  //       port: 587,
+  //       secure: false, // TLS, NOT SSL
+  //       auth: {
+  //         user: env('SMTP_USERNAME'),
+  //         pass: env('SMTP_PASSWORD'),
+  //       },
+  //       tls: {
+  //         rejectUnauthorized: false, // cPanel requires this
+  //       },
+  //     },
+  //     settings: {
+  //       defaultFrom: env('SMTP_FROM'),
+  //       defaultReplyTo: env('SMTP_REPLY_TO'),
+  //     },
+  //   },
+  // },
 });
--- src/plugins/users-permissions/strapi-server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  bootstrap() {
-    console.log("🔥 CUSTOM USERS-PERMISSIONS PLUGIN OVERRIDE LOADED");
-  },
-};
