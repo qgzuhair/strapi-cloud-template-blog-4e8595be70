6e5f25da85d3c577c53dfca4b4f1a22c2a5b7bd8
qgzuhair <zuhair.naqvi@qualitasglobal.com>
2025-11-18 16:59:32 +0530

    Fixing Email Configration

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,20 @@
-module.exports = ({ env }) => ({
-  // email: {
-  //   config: {
-  //     provider: 'nodemailer',
-  //     providerOptions: {
-  //       host: 'md-ht-1.webhostbox.net',
-  //       port: 587,
-  //       secure: false, // TLS, NOT SSL
-  //       auth: {
-  //         user: env('SMTP_USERNAME'),
-  //         pass: env('SMTP_PASSWORD'),
-  //       },
-  //       tls: {
-  //         rejectUnauthorized: false, // cPanel requires this
-  //       },
-  //     },
-  //     settings: {
-  //       defaultFrom: env('SMTP_FROM'),
-  //       defaultReplyTo: env('SMTP_REPLY_TO'),
-  //     },
-  //   },
-  // },
-});
+module.exports = {
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      settings: {
+        defaultFrom: process.env.SMTP_FROM,
+        defaultReplyTo: process.env.SMTP_REPLY_TO,
+      },
+    },
+  },
+};
